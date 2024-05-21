9c336e9f5d48d3fae04a25a1f2a8bb4956584cec
RodoGC <147750905+RodoGC@users.noreply.github.com>
2024-05-21 03:15:48 -0500

    Add files via upload

---
+++ Untitled-2.bat
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BabySteps</title>
+  <link href="/dist/output.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div id="root"></div>
+  <script src="../dist/bundle.js"></script>
+</body>
+</html>
